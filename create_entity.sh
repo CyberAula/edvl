580fa4c716318b4c50aab2db90dba8f5a86ed934
anmunoz <andres.max36@gmail.com>
2021-06-02 12:08:28 +0200

    Add supermarket resources

--- create_entity.sh
+++ create_entity.sh
@@ -16,3 +16,30 @@ curl localhost:1026/v2/entities -s -S -H 'Content-Type: application/json' -d @- 
   }
 }
 EOF
+
+curl http://localhost:1026/v2/entities -s -S -H 'Content-Type: application/json' -d @- <<EOF
+{ 
+  "id": "ticket",
+  "type": "ticket",
+  "_id": {
+    "type": "String",
+    "value": 14
+  },
+  "mall": {
+    "type": "String",
+    "value": 1
+  },
+  "date": {
+    "type": "date",
+    "value": "01/14/2016"
+  },
+  "client": {
+    "type": "int",
+    "value": 77014474650
+  },
+  "items": {
+    "type": "object",
+    "value": {}
+  }
+}
+EOF
--- subscribe.sh
+++ subscribe.sh
@@ -28,3 +28,40 @@ curl -v localhost:1026/v2/subscriptions -s -S -H 'Content-Type: application/json
     "throttling": 5
 }
 EOF
+
+curl -v http://localhost:1026/v2/subscriptions -s -S -H 'Content-Type: application/json' -d @- <<EOF
+{
+  "description": "A subscription to get info about the supermarket",
+  "subject": {
+  "entities": [
+    {
+      "id": "ticket",
+      "type": "ticket"
+    }
+  ],
+  "condition": {
+    "attrs": [
+      "_id",
+      "mall",
+      "date",
+      "client",
+      "items"
+    ]
+  }
+  },
+  "notification": {
+  "http": {
+    "url": "http://spark-worker-1:9001"
+  },
+  "attrs": [
+      "_id",
+      "mall",
+      "date",
+      "client",
+      "items"
+  ]
+  },
+  "expires": "2040-01-01T14:00:00.00Z",
+  "throttling": 5
+}
+EOF
